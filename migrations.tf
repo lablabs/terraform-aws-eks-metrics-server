a7ec3cdb5424ddfec25b4b36a712f92590ff9242
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-05-29 12:49:35 +0200

    chore(migrations): cleanup

--- migrations.tf
+++ migrations.tf
@@ -7,15 +7,9 @@ moved {
 moved {
   from = helm_release.this
   to   = module.addon.helm_release.this
-
 }
 
 moved {
   from = helm_release.argo_application
   to   = module.addon.helm_release.argo_application
-}
-
-moved {
-  from = kubernetes_job.helm_argo_application_wait
-  to   = module.addon.kubernetes_job.helm_argo_application_wait
 }
